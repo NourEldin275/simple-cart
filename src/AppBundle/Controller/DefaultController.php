4b0c40ded15f033984af2e5e78acfb024a9fc2e5
Nour Eldin Ali Mohamed <noureldin.ali.m@gmail.com>
2017-03-31 22:37:37 +0200

    Created a page for shop to display all products with add to cart buttons. minor modifications to accommodate this change in  base.html.twig and sidebar.html.twig templates.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,14 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="shop")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        $products = $this->getDoctrine()->getRepository('AppBundle:Product')->findAll();
+
+        return $this->render('shop/shop.html.twig', array(
+            'products' => $products,
+        ));
     }
 }
